c1bd19f5107acc9ffab67b019493c22c986ac907
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 16:51:08 +0500

    event binding nin svelte

--- src/lib/user.svelte
+++ src/lib/user.svelte
@@ -1,15 +1,15 @@
 <script>
-    export let userImage;
-    export let userName;
-    export let userEmail;
-    export let index;
+  export let userImage;
+  export let userName;
+  export let userEmail;
+  export let index;
 </script>
 
-<div class="flex items-center p-4 bg-white rounded-lg shadow-md space-x-4">
-    <p class="text-lg font-semibold text-gray-700">{index + 1}</p>
-    <img src="{userImage}" alt="User Image" class="w-12 h-12 rounded-full border border-gray-300">
-    <div>
-        <p class="text-gray-800 font-medium">{userName}</p>
-        <p class="text-gray-500 text-sm">{userEmail}</p>
-    </div>
+<div class="flex items-center p-4 bg-white rounded-lg shadow-md space-x-4 border border-gray-200">
+  <p class="text-lg font-semibold text-gray-700">{index + 1}</p>
+  <img src="{userImage}" alt="User Image" class="w-12 h-12 rounded-full border border-gray-300">
+  <div>
+    <p class="text-gray-800 font-medium">{userName}</p>
+    <p class="text-gray-500 text-sm">{userEmail}</p>
+  </div>
 </div>
